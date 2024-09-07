7d643a4c42c0eb238fe4587474dd21fb59b16baf
Hyoungjoon Uh <hjking0613@gmail.com>
2024-09-07 15:42:17 +0900

    spring boot 세팅

---
+++ damoaPJT/src/main/java/com/damoa/damoaPJT/DamoaPjtApplication.java
@@ -0,0 +1,13 @@
+package com.damoa.damoaPJT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DamoaPjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DamoaPjtApplication.class, args);
+	}
+
+}
---
+++ damoaPJT/src/test/java/com/damoa/damoaPJT/DamoaPjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.damoa.damoaPJT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DamoaPjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
