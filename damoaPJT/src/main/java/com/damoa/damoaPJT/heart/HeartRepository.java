740daf194839d61e464bcca908cc4e11fee9f7fb
Hyoungjoon Uh <hjking0613@gmail.com>
2024-10-27 15:29:07 +0900

    로그인 화면 로그인 정보 저장 삭제

--- damoaPJT/src/main/java/com/damoa/damoaPJT/heart/HeartRepository.java
+++ damoaPJT/src/main/java/com/damoa/damoaPJT/heart/HeartRepository.java
@@ -3,8 +3,6 @@ package com.damoa.damoaPJT.heart;
 import com.damoa.damoaPJT.entity.Heart;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.awt.print.Pageable;
-import java.util.List;
 import java.util.Optional;
 
 public interface HeartRepository extends JpaRepository<Heart, Integer> {
